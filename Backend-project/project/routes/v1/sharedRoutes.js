ee933ce3e0fc2e8a5b31f9cceb1ffd58be570761
dominionolonilebi <dominionolufunke27@gmail.com>
2024-12-21 12:34:29 +0100

    recently updated

--- Backend-project/project/routes/v1/sharedRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const {healthCheckHandler} = require('../../controllers/v1/sharedController');
-
-const router = express.Router();
-
-router.get('/health', healthCheckHandler);
-
-module.exports = router;
